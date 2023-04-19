8f5a9933c47e3fc05671d421e81bf2ada25bafb5
KaedenOC <komeacarr2@gmail.com>
2023-04-19 13:54:32 -0700

    fixing for forecast functionality

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,7 +3,13 @@ import { Component } from 'react';
 class Weather extends Component {
   render() {
     return (
+        
         <>
+            {
+                this.props.weatherData.map((forecast) => {
+                    return <p>Date: {forecast.date} Forecast: {forecast.description}</p>
+                })
+            }
             {/* <p>{this.props.dateData} {this.props.weatherData}.</p> */}
         </>
           
