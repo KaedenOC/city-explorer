c346dfe9e963ed279385ff05e395881acb1fa2f5
KaedenOC <komeacarr2@gmail.com>
2023-04-20 15:04:46 -0700

    seperate movies components and add a form file

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,24 +1,14 @@
 import { Component } from 'react';
 import { Carousel } from 'react-bootstrap';
+import Movie from './Movie';
 
 class Movies extends Component {
   render() {
     return (
       <Carousel interval="3000">
         {this.props.movieData.map((obj, idx) => (
-          <Carousel.Item key={idx}>
-            <img
-              className="d-block w-100"
-              src={`https://image.tmdb.org/t/p/w500${obj.imgURL}`}
-              alt={obj.title}
-              // style={{ maxHeight: '500px', objectFit: 'contain' }}
-            />
-            <Carousel.Caption>
-              <h2>{obj.title}</h2>
-              <p>{obj.overview}</p>
-            </Carousel.Caption>
+          <Movie key={idx} movieData={obj}/>
 
-          </Carousel.Item>
         ))}
       </Carousel>
     )
